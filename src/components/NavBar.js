84b5383bea9a7ba7f26fd43de12709634f12527b
Elvis <s49188705@gmail.com>
2022-09-20 11:13:20 +0300

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,3 @@
-// import React from "react";
-
-// function NavBar() {
-//   const links = ["home", "about", "projects"];
-
-//   return (
-//     <nav>
-//       <a href="#home">Home</a>
-//       <a href="#about">About</a>
-//       <a href="#projects">Projects</a>
-//     </nav>
-//   );
-// }
-
-// export default NavBar;
-
 import React from "react";
 
 function NavBar() {
